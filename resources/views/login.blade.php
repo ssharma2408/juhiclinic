bf749ade68a6543ac9567b22901a9b9029039121
Jabar Singh <jsingh@plusonex.com>
2023-09-22 14:46:37 +0530

    login page welcome text changed

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,59 +1,56 @@
 @extends('layouts.layout')
 
 @section('page')
-
-<div>Sign In</div>
-
+    <div>Sign In</div>
 @endsection
 
 @section('content')
+    <div class="text-center">
+        <div class="page-title my-4">
+            <h2> Welcome to {{ $_ENV['APP_NAME'] }}</h2>
+        </div>
 
-<div class="text-center">
-    <div class="page-title my-4">
-        <h2> Welcome to {{$_ENV['DOMAIN']}}</h2>
     </div>
-   
-</div>
-<div class="row">
-    <div class="col-md-5 mx-auto">
-        <div class="card-box text-center">
-            <div class="section-title">
-                <h3 class="text-uppercase">Doctor / Staff login</h3>
-                <div class="text-descripstion secondary-text">
-                Please login to your account
-            </div>
-            </div>
-           
-            <div class="form-wrap text-start">
-                <form method="post" action="{{ route('login.perform') }}" class="contact-form-inner">
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                    @include('layouts.partials.messages')
-                    <div class="single-input-wrap form-group mb-3">
-                        <label class="form-label">Email address<span class="star-red">*</span></label>
-                        <input type="text" name="username" class="form-control" value="{{ old('username') }}" placeholder="Enter your email address" required="required" autofocus>
+    <div class="row">
+        <div class="col-md-5 mx-auto">
+            <div class="card-box text-center">
+                <div class="section-title">
+                    <h3 class="text-uppercase">Doctor / Staff login</h3>
+                    <div class="text-descripstion secondary-text">
+                        Please login to your account
                     </div>
-                    @if ($errors->has('username'))
-                    <span class="text-danger text-left">{{ $errors->first('username') }}</span>
-                    @endif
-                    <div class="single-input-wrap form-group mb-3">
-                        <label class="form-label">Password<span class="star-red">*</span></label>
+                </div>
 
-                        <input type="password" class="form-control" name="password" value="{{ old('password') }}" placeholder="Enter your password" required="required">
-                    </div>
-                    @if ($errors->has('password'))
-                    <span class="text-danger text-left">{{ $errors->first('password') }}</span>
-                    @endif
-                    <div class="single-input-wrap form-group mb-3 text-center">
-                        <button class="btn btn-secondary btn-rounded" type="submit">Login</button>
-                    </div>
-                </form>
+                <div class="form-wrap text-start">
+                    <form method="post" action="{{ route('login.perform') }}" class="contact-form-inner">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                        @include('layouts.partials.messages')
+                        <div class="single-input-wrap form-group mb-3">
+                            <label class="form-label">Email address<span class="star-red">*</span></label>
+                            <input type="text" name="username" class="form-control" value="{{ old('username') }}"
+                                placeholder="Enter your email address" required="required" autofocus>
+                        </div>
+                        @if ($errors->has('username'))
+                            <span class="text-danger text-left">{{ $errors->first('username') }}</span>
+                        @endif
+                        <div class="single-input-wrap form-group mb-3">
+                            <label class="form-label">Password<span class="star-red">*</span></label>
+
+                            <input type="password" class="form-control" name="password" value="{{ old('password') }}"
+                                placeholder="Enter your password" required="required">
+                        </div>
+                        @if ($errors->has('password'))
+                            <span class="text-danger text-left">{{ $errors->first('password') }}</span>
+                        @endif
+                        <div class="single-input-wrap form-group mb-3 text-center">
+                            <button class="btn btn-secondary btn-rounded" type="submit">Login</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
 
 @push('js')
-
 @endpush
